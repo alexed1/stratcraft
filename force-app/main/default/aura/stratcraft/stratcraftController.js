be3dbbe9f31f002c4a8d4265a0ea38dac4206fba
Alex Edelstein <aedelstein@salesforce.com>
2017-12-20 14:21:57 +0000

    added apex controller

--- force-app/main/default/aura/stratcraft/stratcraftController.js
+++ force-app/main/default/aura/stratcraft/stratcraftController.js
@@ -1,5 +1,37 @@
 ({
     myAction : function(component, event, helper) {
 
-    }
+    },
+
+    handleUploadFinished: function (cmp, event) {
+        // Get the list of uploaded files
+        var uploadedFiles = event.getParam("files");
+        alert("Files uploaded : " + uploadedFiles.length);
+    },
+
+    onDrop: function(component, event) { 
+		event.stopPropagation(); 
+		event.preventDefault(); 
+
+		var files = event.dataTransfer.files; 
+		for (var i=0; i<files.length; i=i+1) { 
+		var file = files[i]; 
+		var reader = new FileReader(); 
+		
+		reader.onloadend = function(component, event) { 
+			console.log('loaded');
+			console.log("uploaded file data is: " + reader.result); 		
+			component.set("v.strategyXML", reader.result);
+		
+		}; 
+
+		reader.readAsText(file); 
+		} 
+	}, 
+
+	onDragOver: function(component, event) { 
+		event.preventDefault(); 
+	}, 
+ 
+
 })
