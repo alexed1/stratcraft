9335d612e1f887fa0fc89aa1db1ffc19a19ead95
vlad0203 <vlad.petryshyn1995@gmail.com>
2017-12-31 18:11:43 +0200

    Add files via upload
    Line indent fixing

--- force-app/main/default/aura/stratcraft/stratcraftController.js
+++ force-app/main/default/aura/stratcraft/stratcraftController.js
@@ -1,7 +1,7 @@
 ({
-	init : function(component, event, helper){
+    init : function(component, event, helper){
 
-	},
+    },
 
     handleUploadFinished: function (cmp, event) {
         // Get the list of uploaded files
@@ -10,57 +10,56 @@
     },
 
     onDrop: function(cmp, event, helper) { 
-		event.stopPropagation(); 
-		event.preventDefault(); 
-		var reader = new FileReader();
-		var files = event.dataTransfer.files; 
-	    var spinner = cmp.find("mySpinner");
-      	$A.util.toggleClass(spinner, "slds-hide");
-		for (var i = 0; i < files.length; i++) { 
-			var file = files[i]; 
-			reader = new FileReader(); 		
-			reader.onloadend = function() { 
-				console.log("uploaded file data is: " + reader.result); 		
-				cmp.set("v.strategyXML", reader.result);	
-				var cmpEvent = cmp.getEvent("loadStrategy");
-		        cmpEvent.fire();
-			}; 
-			reader.readAsText(file); 
-		}
-	}, 
+        event.stopPropagation(); 
+        event.preventDefault(); 
+        var reader = new FileReader();
+        var files = event.dataTransfer.files; 
+        var spinner = cmp.find("mySpinner");
+        $A.util.toggleClass(spinner, "slds-hide");
+        for (var i = 0; i < files.length; i++) { 
+            var file = files[i]; 
+            reader = new FileReader(); 
+            reader.onloadend = function() { 
+                console.log("uploaded file data is: " + reader.result);
+                cmp.set("v.strategyXML", reader.result);
+                var cmpEvent = cmp.getEvent("loadStrategy");
+                cmpEvent.fire();
+            }; 
+            reader.readAsText(file); 
+        }
+    }, 
 
     loadStrategy : function (cmp) {
-		var action = cmp.get("c.parseStrategyString");
-		action.setParams({ xml : cmp.get("v.strategyXML") });
-		action.setCallback(this, function(response) {
-			var state = response.getState();
-			if (cmp.isValid() && state === "SUCCESS") {
-				var result = response.getReturnValue();
-				cmp.set("v.curStrat", result);
-				console.log(result.name);
-				result.nodes.forEach(function(entry){
-					console.log(entry.name);
-					console.log(entry.description);
-					console.log(entry.definition);
-					console.log(entry.type);
-				});
-				console.log(result);
-			}
-		    var spinner = cmp.find("mySpinner");
-      		$A.util.toggleClass(spinner, "slds-hide");            
-	  	});
-	  
-      $A.enqueueAction(action);
+        var action = cmp.get("c.parseStrategyString");
+        action.setParams({ xml : cmp.get("v.strategyXML") });
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            if (cmp.isValid() && state === "SUCCESS") {
+                var result = response.getReturnValue();
+                cmp.set("v.curStrat", result);
+                console.log(result.name);
+                result.nodes.forEach(function(entry){
+                    console.log(entry.name);
+                    console.log(entry.description);
+                    console.log(entry.definition);
+                    console.log(entry.type);
+                });
+                console.log(result);
+            }
+            var spinner = cmp.find("mySpinner");
+            $A.util.toggleClass(spinner, "slds-hide");
+        });
+        $A.enqueueAction(action);
     },
 
 
     createStrategyNode : function(component, event) { 
-		
-	}, 
 
-	onDragOver: function(component, event) { 
-		event.preventDefault(); 
-	}, 
+    }, 
+
+    onDragOver: function(component, event) { 
+        event.preventDefault(); 
+    }, 
 
 
 
