c4fd299b821d3e78c6c4404a98e70f9e7e174948
tehnetaholic <teh.netaholic@gmail.com>
2018-01-30 17:43:59 +0300

    ST-6: object and field selection

--- force-app/main/default/aura/criterion/criterionController.js
+++ force-app/main/default/aura/criterion/criterionController.js
@@ -1,22 +1,58 @@
 ({
 
     doInit: function (cmp, event, helper) {
-        // var opts = [
-        //     { value: "Red", label: "Red" },
-        //     { value: "Green", label: "Green" },
-        //     { value: "Blue", label: "Blue" }
-        // ];
-        // component.set("v.objects", opts);
         var action = cmp.get("c.getAvailableObjects");
         action.setCallback(this, function (response) {
             var state = response.getState();
             if (cmp.isValid() && state === "SUCCESS") {
                 var result = response.getReturnValue();
+
+                var emptySelectionObject = { 'name': '', 'label': '--None--' };
+
+                result.map((obj) => {
+                    obj.selected = false;
+                    obj.fields.map((f) => {
+                        f.selected = false;
+                        return f;
+                    });
+                    obj.fields.splice(0, 0, emptySelectionObject)
+                    return obj;
+                })
+
+                result.splice(0, 0, emptySelectionObject);
                 cmp.set("v.objects", result);
             }
         });
 
         $A.enqueueAction(action);
+    },
+
+
+    handleObjectChange: function (cmp, event, helper) {
+        var selectedObject = cmp.get("v.selectedObjectName");
+        var objects = cmp.get("v.objects");
+        var obj = objects.find(function (o) { return o.name == selectedObject });
+
+        if (selectedObject != "")
+            cmp.set("v.fields", obj.fields);
+        else {
+            cmp.set("v.fields", []);
+        }
+
+        cmp.set("v.selectedFieldName", '');
+    },
+
+    handleFieldChange: function (cmp, event, helper) {
+        var selectedField = cmp.get("v.selectedFieldName");
+        console.log(selectedField);
+    },
+
+    doneRendering: function (cmp, event, helper) {
+        if (!cmp.get("v.isDoneRendering") && cmp.get("v.objects").length > 0) {
+            cmp.set("v.isDoneRendering", true);
+            // cmp.find("objectSelect").set("v.value", "");
+            // cmp.set("v.selectedObject", "");
+        }
     }
 
 })
