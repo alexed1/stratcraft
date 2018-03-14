81e65a406f14321a05f92f92cfd6aa8db2f88ee3
Alex Edelstein <alex@edelstein.org>
2018-03-13 17:43:06 -0700

    Revert "ST-50 Expression builder free text mode"

--- force-app/main/default/aura/Criterion/criterion/criterionHelper.js
+++ force-app/main/default/aura/Criterion/criterion/criterionHelper.js
@@ -1,8 +1,83 @@
 ({
-    loadAvailableFields: function (component) {
-        var availableObjects = component.get('v.availableObjects');
-        var objectName = component.get('v.value.objectName');
-        var object = availableObjects.filter(function (item) { return item.name === objectName; })[0];
-        component.set('v.availableFields', object.fields);
-    }
+	assembleCriterion: function (cmp, event, helper, objectName, fieldName, op, textValue) {
+
+		var operator = helper.unifyOperators(op);
+		if (operator != '==' && operator != '!=')
+			operator = '&' + operator + ';';
+
+		var str = '$Record.' + objectName + '.' + fieldName + ' ' + operator + ' ' + textValue;
+		cmp.set('v.criterionValue', str);
+	},
+
+	initExistingCriterion: function (cmp, helper) {
+
+		var criterion = cmp.get('v.criterionValue');
+		if (!criterion)
+			return false;
+		if (criterion === '$true')
+			return false;
+		//example: $Record.Strategy__c.LastModifiedById &amp;gt; (435)
+		try {
+
+			var tokens = criterion.split(' ');
+
+			var objectAndField = tokens[0];
+			var operatorName = tokens[1];
+
+			//if right side of equation had a whitespace, then it got splitted. glue it back
+			var textValue = tokens.splice(2).join(' ');
+
+			var objectName = objectAndField.substring('$Record.'.length, objectAndField.indexOf('.', '	$Record.'.length));
+			var fieldName = objectAndField.substring(objectAndField.lastIndexOf('.') + 1);
+
+			operatorName = helper.unifyOperators(operatorName);
+
+			cmp.set('v.selectedObjectName', objectName);
+			cmp.set('v.selectedFieldName', fieldName);
+			cmp.set('v.selectedOp', operatorName);
+			cmp.set('v.rightSideValue', textValue);
+		}
+		catch (e) {
+			throw new Error('Couldn\'t parse existing criterion', e);
+		}
+		return true;
+	},
+
+	resetCriterion: function (cmp) {
+		cmp.set('v.criterionValue', '');
+	},
+
+	notifyCriterionValueUpdate: function (cmp) {
+		var cmpEvent = $A.get('e.c:criterionUpdatedEvent');
+		cmpEvent.setParams({
+			'criterion': cmp.get('v.criterionValue')
+		});
+		cmpEvent.fire();
+	},
+
+	unifyOperators: function (op) {
+		var trimmedOp = op.replace('&', '').replace(';', '');
+		switch (trimmedOp) {
+			case 'eq':
+			case '==':
+				return '==';
+			case 'nq':
+			case '!=':
+				return '!=';
+			case 'lt':
+			case '<':
+				return 'lt';
+			case 'gt':
+			case '>':
+				return 'gt';
+			case 'lte':
+			case '=<':
+				return 'lte';
+			case 'gte':
+			case '>=':
+				return 'gte';
+			default:
+				throw new Error('Can\'t parse operator: ' + op);
+		}
+	}
 })
--- force-app/main/default/aura/ExpressionBuilder/expressionBuilder/expressionBuilderHelper.js
+++ force-app/main/default/aura/ExpressionBuilder/expressionBuilder/expressionBuilderHelper.js
@@ -1,116 +1,37 @@
 ({
-    splitExpressionIntoSubexpressions: function (expression) {
+
+    assembleCriteriaFromExpression: function (cmp, expression) {
+
         var criteria = [];
-        try {
-            if (expression) {
-                var expressionsArr = expression.split('$');
-                for (var i = 0; i < expressionsArr.length; i++) {
-                    var str = expressionsArr[i];
-                    if (str != null) {
-                        str = str.replace('&&', '');
-                        str = str.replace('||', '').trim();
-                        if (str) {
-                            criteria.push('$' + str);
-                        }
+
+        if (expression) {
+
+            var expressionsArr = expression.split('$');
+            for (var i = 0; i < expressionsArr.length; i++) {
+                var str = expressionsArr[i];
+                if (str != null) {
+                    str = str.replace('&&', '');
+                    str = str.replace('||', '').trim();
+                    if (str != '') {
+                        var newCriterion = {};
+                        newCriterion.criterionValue = '$' + str;
+                        criteria.push(newCriterion);
                     }
                 }
             }
         }
-        catch (error) {
-            console.log('Failed to parse expression "' + expression + "'. Error - " + JSON.stringify(error));
-            return null;
-        }
-        return criteria;
+
+        if (criteria.length == 0)
+            criteria.push({});
+
+        cmp.set("v.criteria", criteria);
     },
 
-    unifyOperators: function (op) {
-        var trimmedOp = op.replace('&', '').replace(';', '');
-        switch (trimmedOp) {
-            case 'eq':
-            case '==':
-                return '==';
-            case 'nq':
-            case '!=':
-                return '!=';
-            case 'lt':
-            case '<':
-                return '<';
-            case 'gt':
-            case '>':
-                return '>';
-            case 'lte':
-            case '=<':
-                return '=<';
-            case 'gte':
-            case '>=':
-                return '>=';
-            default:
-                throw new Error('Can\'t parse operator: ' + op);
-        }
-    },
-    /**Converts a single subexpression into a criteria. Returns null of subexpression can't be converted */
-    convertSubexpressionToCrteria: function (subexpression) {
-        self = this;
-        if (!subexpression || subexpression === 'true' || subexpression === '$true') {
-            return {
-                objectName: '',
-                fieldName: '',
-                selectedOp: 'eq',
-                value: ''
-            };
-        }
-        //example: $Record.Strategy__c.LastModifiedById &amp;gt; (435)
-        try {
-            var tokens = subexpression.split(' ');
-            var objectAndField = tokens[0];
-            var operatorName = tokens[1];
-            //if right side of equation had a whitespace, then it got splitted. glue it back
-            var textValue = tokens.splice(2).join(' ');
-            var objectName = objectAndField.substring('$Record.'.length, objectAndField.indexOf('.', '	$Record.'.length));
-            var fieldName = objectAndField.substring(objectAndField.lastIndexOf('.') + 1);
-            operatorName = self.unifyOperators(operatorName);
-            return {
-                objectName: objectName,
-                fieldName: fieldName,
-                selectedOp: operatorName,
-                value: textValue
-            };
-        }
-        catch (error) {
-            console.log('Can\'t parse subexpression "' + subexpression + '". Error - ' + JSON.stringify(error));
-            return null;
-        }
-    },
-    /**Converts a string expression into a set of criteria objects. Returns null if expression can't be converted*/
-    rebuildCriteriaFromExpression: function (expression) {
-        if (!expression || expression === 'true') {
-            return [];
-        }
-        self = this;
-        var result = [];
-        var isFailed = false;
-        var subexpressions = self.splitExpressionIntoSubexpressions(expression);
-        if (!subexpressions) {
-            return null;
-        }
-        subexpressions.forEach(function (subexpression) {
-            var criteria = self.convertSubexpressionToCrteria(subexpression);
-            if (!criteria) {
-                isFailed = true;
-                return;
-            }
-            result.push(criteria);
-        });
-        if (isFailed) {
-            return null;
-        }
-        return result;
-    },
 
     //called when one of criterion value changes
-    updateExpression: function (component, event, helper) {
-        var criteria = component.get('v.criteria');
-        var result = criteria.map(function (item) { item.criterionValue; }).join(' ');
-        component.set('v.expression', result);
+    updateExpression: function (cmp, event, helper) {
+        var criteria = cmp.get("v.criteria");
+        var result = criteria.map((item) => item.criterionValue).join(' ');
+        cmp.set("v.expression", result);
     }
 })
