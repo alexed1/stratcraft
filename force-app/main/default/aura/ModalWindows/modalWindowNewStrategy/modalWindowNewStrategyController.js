3eac418e8669d0646b7e2e37b6e2949f7ba4cf17
Andrei Chaplygin <gotacoin@gmail.com>
2018-05-12 22:48:12 -0700

    Deleted 2 components

--- force-app/main/default/aura/ModalWindows/modalWindowNewStrategy/modalWindowNewStrategyController.js
+++
@@ -1,6 +0,0 @@
-({
-    validate: function (cmp, event, helper) {
-        var validation = [helper.validateName(cmp), helper.validateLabel(cmp), helper.validateDescription(cmp)];
-        return validation.reduce(function (validSoFar, valid) { return validSoFar && valid; }, true);
-    }
-})
