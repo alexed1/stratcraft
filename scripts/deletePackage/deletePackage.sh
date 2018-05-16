3cdc38a0aa204a1e0309d19d572512fb36817bf3
tehnetaholic <teh.netaholic@gmail.com>
2018-05-16 19:57:14 +0300

    ST-129
    adjustments to cleanup script

--- scripts/deletePackage/deletePackage.sh
+++ scripts/deletePackage/deletePackage.sh
@@ -8,4 +8,6 @@ sfdx force:mdapi:deploy --deploydir ./scripts/deletePackage/deleteTabs/ -u $1 -w
 echo 'Removing permission set assignments'
 sfdx force:apex:execute -f ./scripts/deletePackage/removePermissionset.apex -u $1
 echo 'Removing permission set, Lightning, Apex and static resources'
-sfdx force:mdapi:deploy --deploydir ./scripts/deletePackage/deleteEverythingElse -u $1 -w 5
+sfdx force:mdapi:deploy --deploydir ./scripts/deletePackage/deleteEverythingElse -u rutherford@gs0.org -w 5 -g
+
+$SHELL
