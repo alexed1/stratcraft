54ba66befb243729f6d6140dd625022f86773d21
tehnetaholic <teh.netaholic@gmail.com>
2018-05-16 21:07:50 +0300

    ST-129: another fix

--- scripts/deletePackage/deletePackage.sh
+++ scripts/deletePackage/deletePackage.sh
@@ -4,7 +4,7 @@ if [[ $# -eq 0 ]] ; then
 fi
 
 echo 'Removing tabs'
-sfdx force:mdapi:deploy --deploydir ./scripts/deletePackage/deleteTabs/ -u $1 -w 5
+sfdx force:mdapi:deploy --deploydir ./scripts/deletePackage/deleteTabs/ -u $1 -w 5 -g
 echo 'Removing permission set assignments'
 sfdx force:apex:execute -f ./scripts/deletePackage/removePermissionset.apex -u $1
 echo 'Removing permission set, Lightning, Apex and static resources'
