4410f8d7b942478abfc2bb3e1d81f663efc7af9b
tehnetaholic <teh.netaholic@gmail.com>
2018-07-31 22:37:27 +0300

    LEX-5: right click menu for ExternalConnections & argument name validation

--- force-app/main/default/aura/argumentPair/argumentPairController.js
+++ force-app/main/default/aura/argumentPair/argumentPairController.js
@@ -1,6 +1,22 @@
 ({
-     handleDeleteRequest: function (component, event, helper) {
+    handleDeleteRequest: function (component, event, helper) {
         var cmpEvent = component.getEvent('deleteRequested');
         cmpEvent.fire();
+    },
+
+    validate: function (cmp, event, helper) {
+        var argNameCmp = cmp.find('argName');
+        var argNameError = cmp.find('argNameError');
+        var argName = (cmp.get('v.currentItem.name') || '').trim();
+        var isArgNameValid = argName && !argName.match(/\s+/);
+        _cmpUi.toggleError(argNameCmp, argNameError, isArgNameValid);
+
+        return isArgNameValid;
+    },
+
+    clearValidation: function (cmp, event, helper) {
+        var argNameCmp = cmp.find('argName');
+        var argNameLabelCmp = cmp.find('argNameError');
+        _cmpUi.toggleError(argNameCmp, argNameLabelCmp, true);
     }
 })
--- force-app/main/default/aura/externalConnectionsList/externalConnectionsListController.js
+++ force-app/main/default/aura/externalConnectionsList/externalConnectionsListController.js
@@ -1,10 +1,17 @@
 ({
     handleCurrentStrategyChanged: function (cmp, event, helper) {
+        var self = this;
         var strategy = cmp.get('v.currentStrategy');
-        var container = cmp.find('container');
+        var container = cmp.find('connectionsContainer');
         if (strategy && strategy.externalConnections && strategy.externalConnections.length > 0) {
             $A.util.removeClass(container, 'slds-hide');
             cmp.set('v._externalConnections', strategy.externalConnections);
+            if (cmp.get("v._contextMenuInited") == false) {
+                var pollingId = window.setInterval(
+                    $A.getCallback(function () {
+                        helper.delayedContextMenuInitialisation(cmp, pollingId);
+                    }), 500);
+            }
         } else {
             $A.util.addClass(container, 'slds-hide')
             cmp.set('v._externalConnections', []);
